99b25148221587aa49d398519fe70c851b3452d1
p134c0d3 <70922864+p134c0d3@users.noreply.github.com>
2024-03-20 21:41:27 -0500

    CSS Restructuring

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,17 +2,19 @@ import { Component } from '@angular/core';
 import { WorkoutsComponent } from '../workouts/workouts.component';
 import { FoodComponent } from '../food/food.component';
 import { WaterComponent } from '../water/water.component';
+import { ProfileComponent } from "../profile/profile.component";
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [
-    WorkoutsComponent,
-    FoodComponent,
-    WaterComponent
-  ],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+    selector: 'app-home',
+    standalone: true,
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.css',
+    imports: [
+        WorkoutsComponent,
+        FoodComponent,
+        WaterComponent,
+        ProfileComponent
+    ]
 })
 export class HomeComponent {
 
